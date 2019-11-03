e3f362e820653a4987501c2d8396425fcbc113c0
Jimmy Zeng <jimmyzeng2001@gmail.com>
2019-11-02 22:17:34 -0400

    graph creation

---
+++ graphCreator.py
@@ -0,0 +1,27 @@
+
+class Node(object):
+    # nodes is the connections, dist are the distances
+    # to the connections
+    def __init__(self, name, nodes, dist):
+        self.name = name
+        self.nodes = nodes
+        self.dist = dist
+
+class GraphCreator(object):
+
+    @staticmethod
+    def createGraph():
+        morewood = Node("Morewood", [], [2,4,1])
+        gates
+        newellSimon 
+        wean
+        doherty
+
+        graph = []
+        graph.append(morewood.name)
+        return graph
+
+class drawing
+    def draw(list of names)
+    find the correctsponding node objects
+    draw each object according to x and y
