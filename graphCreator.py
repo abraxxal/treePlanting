729b8d1f01e00c007b03f42031c5432ae23d7985
Jimmy Zeng <jimmyzeng2001@gmail.com>
2019-11-03 01:34:43 -0400

    Added path finding, using node lib TBD

--- graphCreator.py
+++ graphCreator.py
@@ -2,67 +2,6 @@ from cmu_112_graphics import *
 from PIL import Image
 from tkinter import *
 
-class Node(object):
-    # Name is the display name
-    # nodes is the connections, dist are the distances
-    # to the connections
-    def __init__(self, name, nodes=[], dist=[], x=0, y=0):
-        self.name = name
-        self.nodes = nodes
-        self.dist = dist
-        self.x = x
-        self.y = y
-
-    def addNodes(self, nodes):
-        for node in nodes:
-            self.nodes.append(node)
-
-    def addDists(self, dists):
-        for dist in dists:
-            self.dist.append(dist)
-    
-class GraphCreator(object):
-
-    @staticmethod
-    def createGraph():
-        morewood = Node("Morewood E Tower", 300, 50)
-        gates = Node("Gates Center", 300, 100)
-        newellSimon = Node("Newell Simon Hall", 200, 130)
-        wean = Node("Wean Hall", 200, 300)
-        doherty = Node("Doherty Hall", 300, 300)
-
-        morewood.addNodes([gates])
-        morewood.addDists([2])
-        gates.addNodes([newellSimon, morewood])
-        gates.addDists([1, 2])
-        newellSimon.addNodes([gates, wean])
-        newellSimon.addDists([1, 1])
-        wean.addNodes([newellSimon, doherty])
-        wean.addDists([1, 2])
-        doherty.addNodes([wean, gates])
-        doherty.addDists([2, 4])
-
-        graph = {}
-        graph['morewood'] = morewood
-        graph['gates'] = gates
-        graph['newellSimon'] = newellSimon
-        graph['wean'] = wean
-        graph['doherty'] = doherty
-        
-        return graph
-
-'''class drawing
-    def draw(list of names)
-    find the correctsponding node objects
-    draw each object according to x and y'''
-
-class Dot(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.r = 10
-        self.color = "red"
-
 class ShortestPathApp(App):
     def appStarted(app):
         #app.graph = GraphCreator.createGraph()
@@ -73,7 +12,10 @@ class ShortestPathApp(App):
         app.timerDelay = 20
         app.dotList = []
         app.coordList = []
+        app.pathColor = "red2"
+        app.pathWidth = 10
         
+        app.setPathKey()
         app.locationsDict = {
             "mudge": (439, 61),
             "stever":(444, 134),
@@ -90,8 +32,8 @@ class ShortestPathApp(App):
             "doherty entrance": (334, 553),
             "doherty": (251, 530),
             "wean": (166, 510),
-            "scott hall": (116, 499),
-            "hamerschlag": (122, 553),
+            "scott": (116, 499),
+            "hamerschlag hall": (122, 553),
             "west mall": (156, 562),
             "east mall": (320, 605),
             "scaife": (56, 609),
@@ -112,9 +54,19 @@ class ShortestPathApp(App):
             "uc north": (472, 351),
             "aepi": (427, 288),
             "ecg east": (547, 354),
-            "ecg west": (725, 404),
+            "ecg west": (725, 404)
+            }
 
-        }
+    #app.nodes
+    #node in app.nodes
+    #node.x
+    #node.y
+
+    ##############################################
+    #             Change the keys here           #
+    ##############################################
+    def setPathKey(app, keys = ["morewood", "etower", "wts", "purnell"]):
+        app.pathKeys = keys
 
     def getBackground(app):
         url = "https://i.imgur.com/Js6Yr8T.png"
@@ -127,22 +79,8 @@ class ShortestPathApp(App):
     def mousePressed(app, event):
         app.scrollLocation = (event.x, event.y)
         
-        #print(event.x, event.y)
-        
         app.dotList.append(Dot(event.x, event.y))
         app.coordList.append((event.x, event.y))
-
-    '''def trackDrag(app, event):
-        x1, y1 = app.scrollLocation
-        x2, y2 = event.x, event.y
-        app.scrollX -= (x2 - x1)
-        app.scrollY -= (y2 - y1)'''
-
-    #def mouseDragged(app, event):
-    #    app.trackDrag(event)
-    
-    #def mouseReleased(app, event):
-    #    app.trackDrag(event)
 
     def keyPressed(app, event):
         if(event.key == "f" and len(app.dotList) > 0):
@@ -160,19 +98,25 @@ class ShortestPathApp(App):
             canvas.create_oval(dot.x - dot.r, dot.y - dot.r,
                             dot.x + dot.r, dot.y + dot.r,
                             fill = dot.color)
+        
+        for i in range(len(app.pathKeys) - 1):
+            app.drawPath(canvas, app.pathKeys[i], app.pathKeys[i+1])
 
-        app.drawDict(canvas)
-
-    def drawDict(app, canvas):
+        app.drawNodes(canvas)
+    
+    def drawPath(app, canvas, node1, node2):
+        x1, y1 = app.locationsDict[node1]
+        x2, y2 = app.locationsDict[node2]
+        canvas.create_line(x1, y1, x2, y2,
+                           fill = app.pathColor,
+                           width = app.pathWidth)
+    
+    def drawNodes(app, canvas):
         for key in app.locationsDict:
             x, y = app.locationsDict[key]
-
-            if(key == "uc south"):
-                color = "blue"
-            else:
-                color = "green2"
-            canvas.create_oval(x - 10, y - 10,
-                            x + 10, y + 10,
-                            fill = color)
+            canvas.create_oval(x - 8, y - 8,
+                               x + 8, y + 8,
+                               fill = app.pathColor,
+                               width = 0)
 
 app = ShortestPathApp(width=800, height=800)
