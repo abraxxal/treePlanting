b81478750ece5797de61b49bb3a5d5ce4ebc4b44
wyan1103 <w.yan8693@gmail.com>
2019-11-03 16:18:10 -0500

    Updated campus map

--- campus_map.py
+++ campus_map.py
@@ -80,156 +80,155 @@ class Graph(object):
         return result
 
 
-# section of campus north of the Fence.
+# section of campus north of the The Fence.
 def createGraph():
     cmuGraph = Graph()
 
     # north campus
-    cmuGraph.addNode('mudge')
-    cmuGraph.addNode('etower')
-    cmuGraph.addNode('morewood')
-    cmuGraph.addNode('stever')
-    cmuGraph.addNode('tepper')
-    cmuGraph.addNode('tepper quad')
+    cmuGraph.addNode('Mudge House')
+    cmuGraph.addNode('Morewood E-Tower')
+    cmuGraph.addNode('Morewood Gardens')
+    cmuGraph.addNode('Stever House')
+    cmuGraph.addNode('Tepper')
+    cmuGraph.addNode('Tepper Quadrangle')
 
     # mid campus
-    cmuGraph.addNode('hamburg')
-    cmuGraph.addNode('cyert')
-    cmuGraph.addNode('hillman')
-    cmuGraph.addNode('nsh')
-    cmuGraph.addNode('gates')
-    cmuGraph.addNode('purnell')
-    cmuGraph.addNode('aepi')
-    cmuGraph.addNode('wts')
-    cmuGraph.addNode('uc east')
-    cmuGraph.addNode('uc north')
-    cmuGraph.addNode('uc west')
-    cmuGraph.addNode('uc south')
-    cmuGraph.addNode('uc center')
-    cmuGraph.addNode('uc sw')
-    cmuGraph.addNode('ecg east')
-    cmuGraph.addNode('ecg west')
-    cmuGraph.addNode('west wing')
-    cmuGraph.addNode('resnik')
-    cmuGraph.addNode('maggie mo north')
+    cmuGraph.addNode('Hamburg Hall')
+    cmuGraph.addNode('Cyert Hall')
+    cmuGraph.addNode('Hillman Center')
+    cmuGraph.addNode('Newell-Simon Center')
+    cmuGraph.addNode('Gates Center')
+    cmuGraph.addNode('Purnell Center')
+    cmuGraph.addNode('AEPi')
+    cmuGraph.addNode('Walking to the Sky')
+    cmuGraph.addNode('UC East')
+    cmuGraph.addNode('UC North')
+    cmuGraph.addNode('UC West')
+    cmuGraph.addNode('UC South')
+    cmuGraph.addNode('UC Center')
+    cmuGraph.addNode('UC Southwest')
+    cmuGraph.addNode('ECG East')
+    cmuGraph.addNode('ECG West')
+    cmuGraph.addNode('West Wing')
+    cmuGraph.addNode('Resnik Hall')
+    cmuGraph.addNode('Margret Morrison North')
 
     # south campus
     cmuGraph.addNode('wean')
-    cmuGraph.addNode('doherty')
-    cmuGraph.addNode('doherty entrance')
-    cmuGraph.addNode('scott')
-    cmuGraph.addNode('hamerschlag hall')
-    cmuGraph.addNode('porter')
-    cmuGraph.addNode('scaife')
-    cmuGraph.addNode('baker')
-    cmuGraph.addNode('baker entrance')
-    cmuGraph.addNode('hunt')
-    cmuGraph.addNode('cfa')
-    cmuGraph.addNode('fence')
-    cmuGraph.addNode('donner')
-    cmuGraph.addNode('maggie mo south')
-    cmuGraph.addNode('schenley')
-    cmuGraph.addNode('east mall')
-    cmuGraph.addNode('west mall')
-    cmuGraph.addNode('scs quad entrance')
+    cmuGraph.addNode('Doherty Hall')
+    cmuGraph.addNode('Doherty Hall Entrance')
+    cmuGraph.addNode('Scott Hall')
+    cmuGraph.addNode('Hamerschlag Hall')
+    cmuGraph.addNode('Porter Hall')
+    cmuGraph.addNode('Scaife Hall')
+    cmuGraph.addNode('Baker Hall')
+    cmuGraph.addNode('Baker Hall Entrance')
+    cmuGraph.addNode('Hunt Library')
+    cmuGraph.addNode('CFA Building')
+    cmuGraph.addNode('The Fence')
+    cmuGraph.addNode('Donner House')
+    cmuGraph.addNode('Margret Morrison South')
+    cmuGraph.addNode('Schenley Park')
+    cmuGraph.addNode('East Mall')
+    cmuGraph.addNode('West Mall')
+    cmuGraph.addNode('SCS Quad Entrance')
 
     # hilly hilly
-    cmuGraph.addNode('scobell')
+    cmuGraph.addNode('Scobell, Happiest Place on Earth')
 
     # connect north campus
-    cmuGraph.addEdge('mudge', ['stever'], [15], [10])
-    cmuGraph.addEdge('stever', ['morewood'], [17], [10])
-    cmuGraph.addEdge('morewood', ['etower'], [18], [10])
-    cmuGraph.addEdge('tepper', ['tepper quad', 'etower'], [4, 28], [10, 10])
-    cmuGraph.addEdge('etower', ['tepper', 'aepi', 'cyert', 'wts'], [29, 9, 15, 12], [10, 10, 10, 10])
-    cmuGraph.addEdge('tepper quad', ['scs quad entrance'], [5], [10])
-    cmuGraph.addEdge('aepi', ['wts'], [14], [10])
+    cmuGraph.addEdge('Mudge House', ['Stever House'], [28], [10])
+    cmuGraph.addEdge('Stever House', ['Morewood Gardens'], [31], [10])
+    cmuGraph.addEdge('Morewood Gardens', ['Morewood E-Tower'], [30], [10])
+    cmuGraph.addEdge('Tepper', ['Tepper Quadrangle', 'Morewood E-Tower'], [18, 56], [10, 10])
+    cmuGraph.addEdge('Morewood E-Tower', ['AEPi', 'Cyert Hall', 'Walking to the Sky'], [17, 28, 27], [10, 10, 10])
+    cmuGraph.addEdge('Tepper Quadrangle', ['Morewood E-Tower', 'SCS Quad Entrance'], [50, 12], [10, 10])
+    cmuGraph.addEdge('AEPi', ['Walking to the Sky'], [25], [10])
 
-    # connect scs quad + walking to the sky
-    cmuGraph.addEdge('scs quad entrance', ['hamburg', 'hillman', 'cyert'], [8, 6, 8], [10, 10, 10])
-    cmuGraph.addEdge('cyert', ['wts', 'hillman'], [17, 10], [10, 5])
-    cmuGraph.addEdge('wts', ['purnell', 'uc west', 'uc north'], [25, 10, 18], [10, 10, 10])
-    cmuGraph.addEdge('hillman', ['gates'], [12], [2])
-    cmuGraph.addEdge('gates', ['nsh', 'purnell'], [14, 20], [3, 10])
-    cmuGraph.addEdge('nsh', ['wean'], [16], [1])
-    cmuGraph.addEdge('purnell', ['doherty entrance', 'fence'], [22, 29], [10, 10])
+    # connect SCS Quad + walking to the sky
+    cmuGraph.addEdge('SCS Quad Entrance', ['Hamburg Hall', 'Hillman Center', 'Cyert Hall', 'Newell-Simon Center'], [24, 25, 31, 45], [10, 6, 5, 10])
+    cmuGraph.addEdge('Cyert Hall', ['Walking to the Sky', 'Hillman Center'], [29, 22], [10, 5])
+    cmuGraph.addEdge('Walking to the Sky', ['Purnell Center', 'UC West', 'UC North'], [43, 26, 30], [10, 10, 10])
+    cmuGraph.addEdge('Hillman Center', ['Gates Center'], [19], [2])
+    cmuGraph.addEdge('Gates Center', ['Newell-Simon Center', 'Purnell Center'], [29, 33], [3, 10])
+    cmuGraph.addEdge('Newell-Simon Center', ['wean'], [33], [1])
+    cmuGraph.addEdge('Purnell Center', ['Doherty Hall Entrance', 'The Fence', 'UC Southwest'], [39, 49, 31], [10, 10, 10])
 
     # connect everything east of the UC
-    cmuGraph.addEdge('uc sw', ['uc south', 'doherty entrance', 'purnell', 'fence'], [2, 27, 20, 22])
-    cmuGraph.addEdge('uc center', ['uc west', 'uc east', 'uc south'], [4, 9, 4], [1, 1, 1])
-    cmuGraph.addEdge('uc north', ['ecg west', 'uc west'], [14, 6], [1, 1])
-    cmuGraph.addEdge('uc west', ['uc sw'], [7], [10])
-    cmuGraph.addEdge('ecg west', ['ecg east', 'uc east'], [31, 20], [1, 8])
-    cmuGraph.addEdge('ecg east', ['resnik'], [30], [10])
-    # cmuGraph.addEdge('uc west', ['uc south', 'uc east'], [17, 19], [1, 1])
-    cmuGraph.addEdge('uc east', ['maggie mo north'], [15], [1, 1])
-    cmuGraph.addEdge('uc south', ['uc sw', 'maggie mo north'], [4, 22], [10, 10])
-    cmuGraph.addEdge('west wing', ['resnik'], [14], [3])
-    cmuGraph.addEdge('resnik', ['donner', 'scobell', 'ecg east'], [12, 25, 30], [10, 10, 10])
-    cmuGraph.addEdge('donner', ['scobell', 'maggie mo south'], [15, 27], [10, 10])
-    cmuGraph.addEdge('scobell', ['maggie mo south'], [27], [10])
-    cmuGraph.addEdge('maggie mo north', ['maggie mo south', 'west wing'], [22, 10], [1, 10])
-    cmuGraph.addEdge('fence', ['maggie mo south', 'cfa'], [23, 16], [10, 10])
+    cmuGraph.addEdge('UC Southwest', ['UC South', 'Doherty Hall Entrance', 'The Fence'], [9, 43, 32], [2, 10, 10])
+    cmuGraph.addEdge('UC Center', ['UC West', 'UC East', 'UC South', 'UC North'], [8, 23, 24, 31], [1, 1, 1, 1])
+    cmuGraph.addEdge('UC North', ['ECG West', 'UC West'], [26, 25], [1, 1])
+    cmuGraph.addEdge('UC West', ['UC Southwest'], [35], [10])
+    cmuGraph.addEdge('ECG West', ['ECG East', 'UC East'], [61, 39], [1, 8])
+    cmuGraph.addEdge('ECG East', ['Resnik Hall'], [59], [10])
+    cmuGraph.addEdge('UC East', ['Margret Morrison North'], [21], [1])
+    cmuGraph.addEdge('UC South', ['Margret Morrison North'], [29], [10])
+    cmuGraph.addEdge('West Wing', ['Resnik Hall'], [31], [3])
+    cmuGraph.addEdge('Resnik Hall', ['Donner House', 'Scobell, Happiest Place on Earth'], [22, 30], [10, 10])
+    cmuGraph.addEdge('Donner House', ['Scobell, Happiest Place on Earth', 'Margret Morrison South'], [19, 43], [10, 10])
+    cmuGraph.addEdge('Scobell, Happiest Place on Earth', ['Margret Morrison South'], [27], [10])
+    cmuGraph.addEdge('Margret Morrison North', ['Margret Morrison South', 'West Wing'], [57, 12], [1, 10])
+    cmuGraph.addEdge('The Fence', ['Margret Morrison South', 'CFA Building'], [47, 24], [10, 10])
 
     # connect the mall
-    cmuGraph.addEdge('wean', ['scott', 'doherty', 'west mall'], [8, 18, 10], [3, 2, 6])
-    cmuGraph.addEdge('doherty', ['doherty entrance', 'baker'], [19, 23], [3, 10])
-    cmuGraph.addEdge('doherty entrance', ['east mall', 'baker'], [12, 30], [10, 10])
-    cmuGraph.addEdge('hamerschlag hall', ['scott', 'west mall'], [10, 10], [2, 8])
-    cmuGraph.addEdge('west mall', ['porter', 'east mall'], [11, 36], [10, 10])
-    cmuGraph.addEdge('east mall', ['baker entrance'], [10], [10])
-    cmuGraph.addEdge('scaife', ['schenley', 'hamerschlag hall'], [18, 13], [10, 2])
-    cmuGraph.addEdge('porter', ['schenley', 'baker'], [10, 19], [2, 1])
-    cmuGraph.addEdge('baker', ['baker entrance'], [18], [1])
-    cmuGraph.addEdge('baker entrance', ['hunt'], [8], [10])
-    cmuGraph.addEdge('hunt', ['cfa'], [11], [10])
+    cmuGraph.addEdge('wean', ['Scott Hall', 'Doherty Hall', 'West Mall'], [25, 33, 20], [3, 2, 6])
+    cmuGraph.addEdge('Doherty Hall', ['Doherty Hall Entrance', 'Baker Hall'], [34, 40], [2, 10])
+    cmuGraph.addEdge('Doherty Hall Entrance', ['East Mall', 'Baker Hall'], [20, 51], [10, 10])
+    cmuGraph.addEdge('Hamerschlag Hall', ['Scott Hall', 'West Mall'], [20, 26], [2, 8])
+    cmuGraph.addEdge('West Mall', ['Porter Hall', 'East Mall'], [22, 64], [10, 10])
+    cmuGraph.addEdge('East Mall', ['Baker Hall Entrance'], [22], [10])
+    cmuGraph.addEdge('Scaife Hall', ['Schenley Park', 'Hamerschlag Hall'], [25, 24], [10, 2])
+    cmuGraph.addEdge('Porter Hall', ['Schenley Park', 'Baker Hall'], [21, 39], [2, 1])
+    cmuGraph.addEdge('Baker Hall', ['Baker Hall Entrance'], [29], [1])
+    cmuGraph.addEdge('Baker Hall Entrance', ['Hunt Library'], [18], [10])
+    cmuGraph.addEdge('Hunt Library', ['CFA Building'], [19], [10])
 
     # add canvas coordinates for all nodes
     locationsDict = {
-        "mudge": (439, 61),
-        "stever": (444, 134),
-        "morewood": (420, 216),
-        "etower": (384, 287),
-        "tepper": (240, 245),
-        "tepper quad": (252, 291),
-        "scs quad entrance": (254, 320),
-        "hamburg": (190, 325),
-        "nsh": (192, 425),
-        "gates": (268, 422),
-        "hillman": (285, 377),
-        "cyert": (333, 341),
-        "wts": (408, 350),
-        "purnell": (352, 453),
-        "doherty entrance": (334, 553),
-        "doherty": (251, 530),
+        "Mudge House": (439, 61),
+        "Stever House": (444, 134),
+        "Morewood Gardens": (420, 216),
+        "Morewood E-Tower": (384, 287),
+        "Tepper": (240, 245),
+        "Tepper Quadrangle": (252, 291),
+        "SCS Quad Entrance": (254, 320),
+        "Hamburg Hall": (190, 325),
+        "Newell-Simon Center": (192, 425),
+        "Gates Center": (268, 422),
+        "Hillman Center": (285, 377),
+        "Cyert Hall": (333, 341),
+        "Walking to the Sky": (408, 350),
+        "Purnell Center": (352, 453),
+        "Doherty Hall Entrance": (334, 553),
+        "Doherty Hall": (251, 530),
         "wean": (166, 510),
-        "scott": (100, 499),
-        "hamerschlag hall": (90, 553),
-        "west mall": (156, 562),
-        "east mall": (320, 605),
-        "scaife": (56, 609),
-        "schenley": (100, 657),
-        "porter": (138, 617),
-        "baker": (227, 634),
-        "baker entrance": (299, 659),
-        "hunt": (346, 657),
-        "cfa": (386, 625),
-        "fence": (398, 571),
-        "donner": (627, 598),
-        "scobell": (676, 615),
-        "resnik": (647, 541),
-        "west wing": (568, 520),
-        "uc south": (450, 492),
-        "uc west": (447, 402),
-        "uc east": (525, 453),
-        "uc north": (490, 351),
-        "uc center": (470, 430),
-        "uc sw": (425, 490),
-        "aepi": (427, 288),
-        "ecg west": (560, 354),
-        "ecg east": (725, 404),
-        "maggie mo north": (524, 511),
-        "maggie mo south": (516, 611)
+        "Scott Hall": (100, 499),
+        "Hamerschlag Hall": (90, 553),
+        "West Mall": (156, 562),
+        "East Mall": (320, 605),
+        "Scaife Hall": (56, 609),
+        "Schenley Park": (100, 657),
+        "Porter Hall": (138, 617),
+        "Baker Hall": (227, 634),
+        "Baker Hall Entrance": (299, 659),
+        "Hunt Library": (346, 657),
+        "CFA Building": (386, 625),
+        "The Fence": (398, 571),
+        "Donner House": (627, 598),
+        "Scobell, Happiest Place on Earth": (676, 615),
+        "Resnik Hall": (647, 541),
+        "West Wing": (568, 520),
+        "UC South": (450, 492),
+        "UC West": (447, 402),
+        "UC East": (525, 453),
+        "UC North": (490, 351),
+        "UC Center": (470, 430),
+        "UC Southwest": (425, 490),
+        "AEPi": (427, 288),
+        "ECG West": (560, 354),
+        "ECG East": (725, 404),
+        "Margret Morrison North": (524, 511),
+        "Margret Morrison South": (516, 611)
     }
     for key, value in locationsDict.items():
         cmuGraph.addCoordinates(key, value[0], value[1])
@@ -239,5 +238,4 @@ def createGraph():
 def testGraph():
     map = createGraph()
     print(map)
-
 
