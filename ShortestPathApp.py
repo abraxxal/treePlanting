179df270e763f436c01ffb595ae8c272461789e9
Jimmy Zeng <jimmyzeng2001@gmail.com>
2019-11-03 14:06:01 -0500

    zoomed in

--- ShortestPathApp.py
+++ ShortestPathApp.py
@@ -7,9 +7,11 @@ import shortestPath as sp
 class ShortestPathApp(App):
     def appStarted(app):
         app.getBackground()
-        #app.scrollX = 0
-        #app.scrollY = 0
-        #app.scrollLocation = (0, 0) code used once for scrolling
+        app.scrollX = 800
+        app.scrollY = 800
+        #app.scrollLocation = (0, 0)
+        app.scaleFactor = 4.0
+
         app.timerDelay = 20
         app.defaultNodeColor = "deep sky blue"
         app.pathColor = "red"
@@ -22,7 +24,6 @@ class ShortestPathApp(App):
         app.userInputTuple = (app.map.nodes['doherty'], app.map.nodes['mudge'])
         app.shortestPath = app.getShortestPath(app.userInputTuple)
 
-    
     def userInput(app):
         str1 = input("Please type in your starting location:")
         str2 = input("Please type in your ending location:")
@@ -40,20 +41,31 @@ class ShortestPathApp(App):
         return sp.getShortestPath(app.map, inputs[0], inputs[1])
 
     def getBackground(app):
-        url = "https://i.imgur.com/Js6Yr8T.png"
+        url = "https://i.imgur.com/WapEwR6.jpg"
         app.image = app.loadImage(url)
-        app.image = app.image.resize((866, 800))
+        #app.image = app.scaleImage(app.image, 4/4)
 
-        app.bgX = app.width / 2 + 30
-        app.bgY = app.width / 2
+        app.bgX = 1600 #app.width / 2
+        app.bgY = 1600 #app.width / 2
 
     def keyPressed(app, event):
-        app.userInputTuple = app.userInput()
-        app.shortestPath = app.getShortestPath(app.userInputTuple)
+        if(event.key == 'Left'):
+            app.scrollX -= 50
+        elif(event.key == 'Right'):
+            app.scrollX += 50
+        elif(event.key == 'Up'):
+            app.scrollY -= 50
+        elif(event.key == 'Down'):
+            app.scrollY += 50
+        #elif(event.key == 'r'):
+        #    app.scaleFactor -= 0.5
+        elif(event.key == 't'):
+            app.userInputTuple = app.userInput()
+            app.shortestPath = app.getShortestPath(app.userInputTuple)
 
     def drawBG(app, canvas):
-        canvas.create_image(app.bgX,
-                            app.bgY,
+        canvas.create_image(app.bgX - app.scrollX,
+                            app.bgY - app.scrollY,
                             image = ImageTk.PhotoImage(app.image))
 
     def drawPath(app, canvas):
@@ -63,8 +75,10 @@ class ShortestPathApp(App):
             app.drawSingleNode(canvas, app.shortestPath[j], app.pathColor)
 
     def drawSegment(app, canvas, node1, node2):
-        x1, y1 = node1.x, node1.y
-        x2, y2 = node2.x, node2.y
+        x1 = node1.x * app.scaleFactor - app.scrollX
+        y1 = node1.y * app.scaleFactor - app.scrollY
+        x2 = node2.x * app.scaleFactor - app.scrollX
+        y2 = node2.y * app.scaleFactor - app.scrollY
         canvas.create_line(x1, y1, x2, y2,
                            fill = app.pathColor,
                            width = app.pathWidth)
@@ -74,8 +88,8 @@ class ShortestPathApp(App):
             app.drawSingleNode(canvas, app.map.nodes[key], app.defaultNodeColor)
 
     def drawSingleNode(app, canvas, node, color):
-            x = node.x 
-            y = node.y
+            x = node.x * app.scaleFactor - app.scrollX 
+            y = node.y * app.scaleFactor - app.scrollY
             canvas.create_oval(x - app.r, y - app.r,
                                x + app.r, y + app.r,
                                fill = color,
@@ -85,5 +99,6 @@ class ShortestPathApp(App):
         app.drawBG(canvas)
         app.drawNodes(canvas)
         app.drawPath(canvas)
+        #canvas.create_rectangle(400,400,250,300, fill = "black")
 
 app = ShortestPathApp(width=800, height=800)
